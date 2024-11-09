aac8c0667ff456ce7b86563a57348df24e43289e
Tim Creasman <tcreasman@pattern.health>
2024-11-09 16:50:08 -0500

    Added basic layout

--- src/app.tsx
+++ src/app.tsx
@@ -2,15 +2,21 @@ import { MetaProvider, Title } from "@solidjs/meta";
 import { Router } from "@solidjs/router";
 import { FileRoutes } from "@solidjs/start/router";
 import { Suspense } from "solid-js";
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
 
 export default function App() {
   return (
     <Router
       root={props => (
-        <MetaProvider>
-          <Title>Joffee - A Local Coffee Finder</Title>
-          <Suspense>{props.children}</Suspense>
-        </MetaProvider>
+        <>
+          <Header />
+          <MetaProvider>
+            <Title>Joffee - A Local Coffee Finder</Title>
+            <Suspense>{props.children}</Suspense>
+          </MetaProvider>
+          <Footer />
+        </>
       )}
     >
       <FileRoutes />
